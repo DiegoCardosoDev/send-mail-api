b65f20928297f57912e111c512900c3b4a92ea84
DiegoCardosoDev <diegocardososacramento@gmail.com>
2022-02-16 20:51:14 -0300

    service method sendEmail implements

--- src/main/java/com/diego/mailsender/services/EmailService.java
+++ src/main/java/com/diego/mailsender/services/EmailService.java
@@ -1,18 +1,47 @@
 package com.diego.mailsender.services;
 
 
+import com.diego.mailsender.enums.StatusEmail;
 import com.diego.mailsender.model.EmailModel;
 import com.diego.mailsender.repositories.EmailRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @AllArgsConstructor
 @Service
 public class EmailService {
 
-    private EmailRepository emailRepository;
+    EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel emailModel) {
+    private JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDataEmail(LocalDateTime.now());
+        try {
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+
+        }catch (MailException exception){
+            log.error("erro ao enviar  email");
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+
+        }finally {
+            return emailRepository.save(emailModel);
+        }
 
 
     }
