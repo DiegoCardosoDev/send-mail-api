d23b8a02c28ba5ec612985b6229e0c0a78dce44f
DiegoCardosoDev <diegocardososacramento@gmail.com>
2022-02-16 15:59:28 -0300

    model implements

---
+++ src/main/java/com/diego/mailsender/model/EmailModel.java
@@ -0,0 +1,32 @@
+package com.diego.mailsender.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "tb_email")
+public class EmailModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime sendDataEmail;
+
+    private StatusEmail statusEmail;
+
+
+
+
+}
