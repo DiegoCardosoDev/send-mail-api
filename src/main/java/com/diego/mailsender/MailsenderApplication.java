8acd41f880ebeeedfb990929d7111ecde94e4f13
DiegoCardosoDev <diegocardososacramento@gmail.com>
2022-02-16 15:42:31 -0300

    init

---
+++ src/main/java/com/diego/mailsender/MailsenderApplication.java
@@ -0,0 +1,13 @@
+package com.diego.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailsenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/mailsender/MailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
